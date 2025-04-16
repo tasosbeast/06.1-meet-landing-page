507d84c56db9ebc5fb7e911641fda0b71c93d55d
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-04-17 01:50:27 +0300

    Enhances button hover effects and animations
    Adds hover background colors for primary and secondary buttons.
    Introduces pointer cursor and smooth transition for better UX.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 function Button({ color, children }) {
   const backgroundColors = {
-    primary: "bg-cyan-600",
-    secondary: "bg-purple-600",
+    primary: "bg-cyan-600 hover:bg-[#71C0D4]",
+    secondary: "bg-purple-600 hover:bg-[#B18BDD]",
   };
   return (
     <button
-      className={`${backgroundColors[color]} px-10 py-4 rounded-4xl red-hat-display-black text-white leading-lg`}
+      className={`${backgroundColors[color]} px-10 py-4 rounded-4xl red-hat-display-black text-white leading-lg hover:cursor-pointer duration-300 ease-in-out`}
     >
       {children}
     </button>
